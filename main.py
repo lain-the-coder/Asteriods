7c406a1d8fe0d145c1a740bc7f81b5b1c997f629
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-03-07 17:24:36 +0400

    implement player shooting
    - Added SHOT_RADIUS and PLAYER_SHOOT_SPEED constants
    - Created Shot class inheriting from CircleShape
    - Added shoot method to Player and mapped to SPACE key
    - Set up shots sprite group and containers in main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,6 +14,8 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (updatable, drawable, shots)
     AsteroidField.containers = (updatable)
     Asteroid.containers = (updatable, drawable, asteroids)
     Player.containers = (updatable, drawable)
